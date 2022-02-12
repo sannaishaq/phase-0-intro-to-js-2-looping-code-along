7f2a1b45ab78457962a91743187d57f4960874b3
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-02-12 10:40:23 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], 'birthday'));
+
+function countDown(counter){
+    let i = counter
+while (i >= 0) {
+    console.log(i);
+    i -=1;
+}
+}
